9ef96f89123544312be6aee19d79ba24b2765daf
Connor G Meehan <connorgm@pm.me>
2024-02-01 22:49:53 +1100

    feat: Adds persisent paddys + completion handler

--- plugin/paddynvim.lua
+++ plugin/paddynvim.lua
@@ -5,6 +5,32 @@ end
 
 _G.PaddyNvimLoaded = true
 
-vim.api.nvim_create_user_command("Paddy", function()
-    require("paddynvim").paddy()
-end, {})
+vim.api.nvim_create_user_command("Paddy", function(opts)
+    print(vim.inspect(opts.fargs))
+    require("paddynvim").paddy(opts.fargs)
+end, {
+    nargs = "*",
+    complete = function (_, line)
+        local args = vim.split(line, "%s+")
+        -- Base command completion handler
+        if #args == 2 then
+            local keys = vim.tbl_keys(_G.PaddyNvim.commands)
+            return vim.tbl_filter(function (val)
+                -- Filter out private fields (completion handlers)
+                if val:startswith("_") then
+                    return false
+                end
+                return vim.startswith(val, args[2])
+            end, keys)
+        end
+        -- Command specific completion handler
+        if #args == 3 then
+            local cmd_name = args[2]
+            local completion_key = "_completion_" .. cmd_name
+            local completion_handler = _G.PaddyNvim.commands[completion_key]
+            if completion_handler then
+                return completion_handler(args)
+            end
+        end
+    end
+})
